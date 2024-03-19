b00fbb8b74a064b7998967bc00aff46327ff1053
sukrusimsek <sukrusimsekll@gmail.com>
2024-03-20 02:08:08 +0300

    first commit

---
+++ LittleLemonDinnerMenu-Meta/LittleLemonDinnerMenu_MetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonDinnerMenu_MetaApp.swift
+//  LittleLemonDinnerMenu-Meta
+//
+//  Created by Şükrü Şimşek on 20.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonDinnerMenu_MetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
